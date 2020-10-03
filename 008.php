6a59ff58678fb82a4cc4ceb312fcd8c07e587528
safeoff <sgmrrc@gmail.com>
2020-10-03 14:47:45 +0900

    fix 008

--- 008.php
+++ 008.php
@@ -1,8 +1,9 @@
 <?php
-    $_GET[t1] = htmlspecialchars($_GET[t1]);
-    $_GET[t1] = preg_replace('/accesskey/i', '#', $_GET[t1]);
+    $_POST[t1] = htmlspecialchars($_POST[t1]);
+    $_POST[t1] = preg_replace('/accesskey/i', '#', $_POST[t1]);
 ?>
-<form action='' method='GET'>
-    <input type='text' name='t1' value='<?php echo $_GET[t1]; ?>'>
+<meta http-equiv="x-ua-compatible" content="IE=10">
+<form action='' method='POST'>
+    <input type='text' name='t1' value='<?php echo $_POST[t1]; ?>'>
     <input type='submit'>
 </form>
